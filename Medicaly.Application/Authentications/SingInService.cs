d405361cbd3d58d216c65588674bfac8d3c25279
RafaelKC <rafaelkaua97@gmail.com>
2024-04-15 20:09:48 -0300

    adicionado autenticação

--- Medicaly.Application/Authentications/SingInService.cs
+++ Medicaly.Application/Authentications/SingInService.cs
@@ -1,4 +1,6 @@
-﻿using Medicaly.Application.Pacientes;
+﻿using Medicaly.Application.Administradores;
+using Medicaly.Application.Pacientes;
+using Medicaly.Application.Profissionals;
 using Medicaly.Application.Transients;
 using Medicaly.Domain.Users.Enums;
 using Medicaly.Infrastructure.Authentication;
@@ -16,24 +18,53 @@ public class SingInService: ISingInService, IAutoTransient
 {
     private readonly IAuthenticationService _authenticationService;
     private readonly IPacienteService _pacienteService;
+    private readonly IProfissionalService _profissionalService;
+    private readonly IAdministradorService _administradorService;
 
-    public SingInService(IAuthenticationService authenticationService, IPacienteService pacienteService)
+    public SingInService(
+        IAuthenticationService authenticationService,
+        IPacienteService pacienteService,
+        IProfissionalService profissionalService,
+        IAdministradorService administradorService)
     {
         _authenticationService = authenticationService;
         _pacienteService = pacienteService;
+        _profissionalService = profissionalService;
+        _administradorService = administradorService;
     }
 
     public async Task<LoginOutput> SingIn(UserTipo tipoUsuario, LoginInput loginInput)
     {
-        if (tipoUsuario == UserTipo.Paciente)
+        switch (tipoUsuario)
         {
-            var paciente = await _pacienteService.GetByEmail(loginInput.email);
-            if (paciente is null)
-                return new LoginOutput
-                {
-                    Success = false
-                };
+            case UserTipo.Administrador:
+                var administrador = await _administradorService.GetByEmail(loginInput.email);
+                if (administrador is null)
+                    return new LoginOutput
+                    {
+                        Success = false
+                    };
+                break;
+            case UserTipo.ProfissionalSaude:
+                var profissional = await _profissionalService.GetByEmail(loginInput.email);
+                if (profissional is null)
+                    return new LoginOutput
+                    {
+                        Success = false
+                    };
+                break;
+            case UserTipo.Paciente:
+                var paciente = await _pacienteService.GetByEmail(loginInput.email);
+                if (paciente is null)
+                    return new LoginOutput
+                    {
+                        Success = false
+                    };
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(tipoUsuario), tipoUsuario, null);
         }
+
         return await _authenticationService.Login(loginInput);
     }
 }
