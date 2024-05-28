aa2f413f752c379c54bf33cfd3e2c34ef659335b
passossss <gusapas26@gmail.com>
2024-05-27 23:35:00 -0300

    AAA

---
+++ Medicaly.Infrastructure/Migrations/20240528015454_Ajustesd.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Medicaly.Infrastructure.Migrations
+{
+    public partial class Ajustesd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ResultadoAnexos_AnexoId",
+                schema: "public",
+                table: "ResultadoAnexos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ResultadoAnexos_ProcedimentoId",
+                schema: "public",
+                table: "ResultadoAnexos");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ResultadoAnexos_ProcedimentoId",
+                schema: "public",
+                table: "ResultadoAnexos",
+                column: "ProcedimentoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ResultadoAnexos_ProcedimentoId",
+                schema: "public",
+                table: "ResultadoAnexos");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ResultadoAnexos_AnexoId",
+                schema: "public",
+                table: "ResultadoAnexos",
+                column: "AnexoId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ResultadoAnexos_ProcedimentoId",
+                schema: "public",
+                table: "ResultadoAnexos",
+                column: "ProcedimentoId",
+                unique: true);
+        }
+    }
+}
